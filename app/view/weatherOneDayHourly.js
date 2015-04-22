9a25989b59962bf9a1387f4dac32e362d056b330
unknown <vkryv@SAGE.softservecom.com>
2015-04-22 18:45:56 +0300

    nnnnn

--- app/view/weatherOneDayHourly.js
+++ app/view/weatherOneDayHourly.js
@@ -2,9 +2,10 @@ Ext.define("Weather.view.weatherOneDayHourly", {
    extend:'Ext.tab.Panel',
 	alias:'widget.aliasweatherOneDayHourly',
 	title: 'Погода',
+    height:500,
     region:'south',
     items:[{
-        title: '5 Днів',
+        
         tpl: new Ext.XTemplate('<tpl for=".">',
                                 '<div class="weather_block">',
                                     '<p>Data: {data}</p>',
@@ -14,12 +15,5 @@ Ext.define("Weather.view.weatherOneDayHourly", {
                                 '</div>',
                                 '</tpl>')
               
-    },{
-        title: '16 днів',
-        html: 'Тут буде погода за 16 днів'
-
-    },{
-        title: 'Історія',
-        html: 'Історія погоди'
     }]
 });
