7c0c092d202a60f4c19447ab303e44aff3e3dd34
unknown <vkryv@SAGE.softservecom.com>
2015-04-22 16:08:43 +0300

    ggg

--- app/view/Center.js
+++ app/view/Center.js
@@ -18,10 +18,10 @@ Ext.define("Weather.view.Center", {
         id: 'weatherFiveDays',
         tpl: new Ext.XTemplate('<tpl for=".">',
                                 '<div class="weather_block">',
-                                    '<h2>Data: {data}</h2>',
-                                    '<p><img src="{icon}"></p>',
-                                    '<p>min-temperature:{max_temperature}</p>',
-                                    '<p>max-temperature:{min_temperature}</p>',
+                                    '<p>Data: {data}</p>',
+                                    '<p><img src="{icon}" width=150></p>',
+                                    '<p><span class="min">min-temperature:</span>{max_temperature}</p>',
+                                    '<p><span class="max">max-temperature:{min_temperature}:</span></p>',
                                 '</div>',
                                 '</tpl>')
               
