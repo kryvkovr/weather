7497001ef0d5dab02fe693ddc94cc6f599d6b795
unknown <vkryv@SAGE.softservecom.com>
2015-04-24 18:23:04 +0300

    add some css

--- app/view/Center.js
+++ app/view/Center.js
@@ -43,7 +43,7 @@ Ext.define("Weather.view.Center", {
                 itemTpl:'<div class="weatherDayHour">'+
                             '<div style="font-size:15px;color:#fff">Hour-{dt}</div>'+
                             '<div style="color:red">temp min {main.temp_min}</div>'+
-                            '<div style="color:green">temp max {main.temp_max}</div>'+
+                            '<div style="color:yellow">temp max {main.temp_max}</div>'+
                             '<img src="http://openweathermap.org/img/w/{weather}.png" width="100"></img>'+
                         '</div>',
 
