a0e5f9a8329c1019d360139c56486ccd0bb8b540
unknown <vkryv@SAGE.softservecom.com>
2015-04-28 18:00:29 +0300

    ggggg

--- app/template/Mytemplate.js
+++ app/template/Mytemplate.js
@@ -1,11 +1,16 @@
 Ext.define('Weather.template.Mytemplate', {
     extend: 'Ext.XTemplate',
 
-    tpl: ['<div>{dt}hello world</div>'],
-    // statics: {
-    //     instanceCount: 'blalalalalaalalalal',
-    // },        
+    tpl: ['<div class="weatherDayBlock">',
+                '<div style="font-size:20px">Day:{dt}</div>',
+                '<div style="color:red">temp max {temp.max}</div>',
+                '<div style="color:green">temp min {temp.min}</div>',
+            '</div>'
+    ],
+        
     constructor: function () {
         this.callParent(this.tpl);
     }
 });
+
+
--- app/view/FiveDaysDaily.js
+++ app/view/FiveDaysDaily.js
@@ -1,6 +1,7 @@
 Ext.define("Weather.view.FiveDaysDaily", {
     id:'weatherDaily',
     alias: 'widget.weather-five-days-daily',
+
     requires:[
         'Weather.template.Mytemplate'
     ],
@@ -11,33 +12,14 @@ Ext.define("Weather.view.FiveDaysDaily", {
     itemSelector: '',
 
     initComponent: function() {
-        
-       // this.itemTpl=2+2;
-        this.tpl=Weather.template.Mytemplate.create({});
-        
+        this.itemTpl=Weather.template.Mytemplate.create({});
         this.callParent(arguments);
     },
-    //tpl:'',
-    //itemSelector:'',
-
-    //xtype: 'dataview',
-
-    
-    //itemTpl: 'fffff',
-    // itemTpl: Weather.template.Mytemplate.instanceCount,
-
-    // itemTpl:new Ext.XTemplate('<div class="weatherDayBlock">',
-    //                                 '<div style="font-size:20px">Day:{dt}</div>',
-    //                                 '<div style="color:red">temp max {temp.max}</div>',
-    //                                 '<div style="color:green">temp min {temp.min}</div>',
-    //                             '</div>'
-    //                         ),                        
+   
     store:'WeatherFiveDaysDaily',
     listeners: {
         'itemclick': function(view, record, item, idx, event, opts){
-          // var me= Ext.create('Weather.template.Mytemplate')
            console.log(Weather.template.Mytemplate.instanceCount)             
-             
         }
     }
 
