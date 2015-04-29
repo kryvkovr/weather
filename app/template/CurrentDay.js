c39b6e8da2e2681105531183dc1f2a4ec17003e5
unknown <vkryv@SAGE.softservecom.com>
2015-04-29 18:19:47 +0300

    aaa

--- app/template/CurrentDay.js
+++ app/template/CurrentDay.js
@@ -1,10 +1,37 @@
 Ext.define('Weather.template.CurrentDay', {
     extend: 'Ext.XTemplate',
 
-    tpl: ['{sys}{weather}{main}'
+    tpl: ['<table>',
+            '<tr>',
+                '<td><img src="http://openweathermap.org/img/w/{icon}.png"></img></td>',
+                '<td><span>{description}</span></td>',
+            '</tr>',
+            '<tr>',
+                '<td>Sunrise</td>',
+                '<td>{sys.sunrise}</td>',
+            '</tr>',
+            '<tr>',
+                '<td>Sunset</td>',
+                '<td>{sys.sunrise}</td>',
+            '</tr>',
+            '<tr>',
+                '<td>Max-temp</td>',
+                '<td>{temp.max}</td>',
+            '</tr>',
+            '<tr>',
+                '<td>Min-temp</td>',
+                '<td>{temp.min}</td>',
+            '</tr>',
+            '<tr>',
+                '<td>Night-temp</td>',
+                '<td>{temp.night}</td>',
+           ' </tr>',
+
+        '</table>',
     ],
         
     constructor: function () {
         this.callParent(this.tpl);
     }
 });
+
