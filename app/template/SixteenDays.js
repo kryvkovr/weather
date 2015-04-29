b285dc2487ab1bb15c445f4a29a0370b2309bebc
unknown <vkryv@SAGE.softservecom.com>
2015-04-29 13:34:25 +0300

    ad separate template

--- app/template/SixteenDays.js
+++ app/template/SixteenDays.js
@@ -1,15 +1,20 @@
-Ext.define('Weather.store.WeatherSixteenDays', {
-    extend: 'Ext.data.Store',
-    model:'Weather.model.SixteenDays',
-    proxy: {
-        type: 'ajax',      
-        reader: {
-            type: 'json',
-            root: 'list'         
-        }
-    } 
-});
+Ext.define('Weather.template.SixteenDays', {
+    extend: 'Ext.XTemplate',
 
+    tpl: ['<div class="sixteenWeatherDayBlock">{dt}',  
+                '<div class="weatherDescription">',
+                    '<img src="http://openweathermap.org/img/w/{weather}.png"></img>',
+                    '<div class="tempMax">Max-temp: {temp.max}</div>',
+                    '<div class="tempMin">Min-temp: {temp.min}</div>',
+                    '<div class="tempNight">Night-temp: {temp.night}</div>',
+                 '</div>',  
+            '</div>'
+    ],
+        
+    constructor: function () {
+        this.callParent(this.tpl);
+    }
+});
 
 
 // itemTpl:new Ext.XTemplate('<div class="sixteenWeatherDayBlock">{dt}',                                      
--- app/view/SixteenDays.js
+++ app/view/SixteenDays.js
@@ -1,30 +1,21 @@
 Ext.define("Weather.view.SixteenDays", {
     extend: 'Ext.view.View',
-    //id:'weatherDaily',
+
+    requires:[
+         'Weather.template.SixteenDays'
+     ],
+
     alias: 'widget.sixteen-days',
     title: '16 DAYS',
     autoScroll: true,  
-    itemTpl:new Ext.XTemplate('<div class="sixteenWeatherDayBlock">{dt}',                                      
-                                        '<div class="weatherDescription">',
-                                            '<img src="http://openweathermap.org/img/w/{weather}.png"></img>',
-                                            '<div class="tempMax">Max-temp: {temp.max}</div>',
-                                            '<div class="tempMin">Min-temp: {temp.min}</div>',
-                                            '<div class="tempNight">Night-temp: {temp.night}</div>',
-                                        '</div>',                                        
-                                '</div>'
-                            ),
-     store:'WeatherSixteenDays'
-    // requires:[
-    //     'Weather.template.FiveDaysDaily'
-    // ],
-
-    // height:200,
-    // itemSelector: '',
-
-    // initComponent: function() {
-    //     this.itemTpl=Weather.template.FiveDaysDaily.create({});
-    //     this.callParent(arguments);
-    // },
+    store:'WeatherSixteenDays',
    
-    //store:'WeatherFiveDaysDaily'
+
+    initComponent: function() {
+        this.itemTpl=Weather.template.SixteenDays.create({});
+        this.callParent(arguments);
+    }
+   
+   
 });
+
