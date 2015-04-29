ad80e89bf7671a761a45b4548cf56ea5399eb189
unknown <vkryv@SAGE.softservecom.com>
2015-04-29 18:34:31 +0300

    aaa

--- app/model/CurrentDay.js
+++ app/model/CurrentDay.js
@@ -3,7 +3,35 @@ Ext.define('Weather.model.CurrentDay', {
     fields: [
         { 	name:'sys', type:'auto'},
         {   name:'weather', type:'auto'},
-        {   name:'main', type:'auto'},       	
+        {   name:'main', type:'auto'},
+        {
+            name:'icon', type:'auto',
+            convert: function (v, record){
+                var weather = record.get('weather')[0];
+                return weather.icon;
+            }
+        },
+        {
+            name:'description',
+            convert: function (v, record){
+                var weather = record.get('weather')[0];
+                return weather.description;
+            }         
+        },
+        {
+            name:'sunrise',
+            convert: function (v, record){
+                var sunSystem = record.get('sys').sunrise;
+                return moment.unix(sunSystem).format('h:mm:ss');
+            }         
+        },
+        {
+            name:'sunset',
+            convert: function (v, record){
+                var sunSystem = record.get('sys').sunset;
+                return moment.unix(sunSystem).format('h:mm:ss');
+            }         
+        }                  	
     ]   
 });
 
