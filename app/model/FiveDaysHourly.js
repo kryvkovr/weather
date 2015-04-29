0dda115e8c0ccd02f2119389f4099499fd70ea86
unknown <vkryv@SAGE.softservecom.com>
2015-04-29 18:51:11 +0300

    complete table

--- app/model/FiveDaysHourly.js
+++ app/model/FiveDaysHourly.js
@@ -1,20 +1,16 @@
 Ext.define('Weather.model.FiveDaysHourly', {
     extend: 'Ext.data.Model',
     fields: [
-        {   name: 'dt' , type:'auto'},           
-           
+        {   name: 'dt' , type:'auto'},                     
         {   name:'main' , type:'auto'},       	
-         
-        {   name: 'weather', type: 'auto'},
-         
+        {   name: 'weather', type: 'auto'},         
         {
             name: 'icon', type:'auto',
             convert: function (v, record){
                 var weather = record.get('weather')[0];
                 return weather.icon;
             }
-        },
-         
+        }, 
         {
             name: 'hour', type:'auto',
             convert: function (v, record){
@@ -22,7 +18,6 @@ Ext.define('Weather.model.FiveDaysHourly', {
                 return moment.unix(data).format('H-00');
             }
         },
-         
         {
             name: 'day', type:'auto',
             convert: function (v, record){
