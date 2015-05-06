d9c8a60818dd7f6713d3fdbab663bdaa74eb345a
unknown <vkryv@SAGE.softservecom.com>
2015-05-06 12:39:40 +0300

    aaaa

--- app/Classes/GetWeatherData.js
+++ app/Classes/GetWeatherData.js
@@ -6,27 +6,28 @@ Ext.define('Weather.Classes.GetWeatherData', {
     var req = new XMLHttpRequest();
     req.open('GET', url);
 
-    req.onload = function() {
+    req.onreadystatechange = function() {
       // This is called even on 404 etc
       // so check the status
-      if (req.status == 200) {
+      if (req.readyState==4 && req.status == 200) {
         // Resolve the promise with the response text
         resolve(req.response);
-      }
-      else {
+
+      }else {
+
         // Otherwise reject with the status text
         // which will hopefully be a meaningful error
-        reject(Error(req.statusText));
+       // reject(Error(req.statusText));
       }
     };
 
     // Handle network errors
-    req.onerror = function() {
-      reject(Error("Network Error"));
-    };
+    // req.onerror = function() {
+    //   reject(Error("Network Error"));
+    // };
 
     // Make the request
-    req.send();
+    req.send(null);
   });
 }
 
